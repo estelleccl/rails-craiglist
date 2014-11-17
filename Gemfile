fedda5db93399fcb37ab51c5ad1fb83724791c04
Esu Chang <Esu@Esus-M.local>
2014-11-17 16:32:57 +0800

    completed most accept css part

--- Gemfile
+++ Gemfile
@@ -38,3 +38,6 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+# Use faker to seed categories
+gem 'faker',        group: :development
+gem 'byebug',        group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require_relative '../app/models/category'
+require_relative '../app/models/post'
+
+class CategoryImporter
+  def self.import
+    categories_list = ["job offered", "gig offered", "resume / job wanted", "housing offered", "housing wanted"]
+    
+    categories_list.each do |name|
+      Category.create(name: name)
+    end
+  end
+end
+
+class PostImporter
+  def self.import
+    20.times do |x|
+      Post.create(
+        title: Faker::Lorem.sentence,
+        description: Faker::Lorem.paragraph, 
+        location: Faker::Address.street_name,
+        postcode: Faker::Address.postcode,
+        poster_name: Faker::Name.name,
+        img_link: Faker::Internet.url,
+        category_id: rand(1..Category.count),
+        key: create_key
+        )
+    end
+  end
+end
+
+def create_key(len=10)
+  SecureRandom.hex(len)
+end
+
+CategoryImporter.import
+PostImporter.import
